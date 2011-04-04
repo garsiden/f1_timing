271c9259e1d4eb81fd96b095c67c2ceeff035064
Nigel Garside <nigel.garside@gmail.com>
2011-04-04 14:38:55 +0100

    Move pdf parsing to separate sub-routine
    Enables the source to be either a a file or the output of pdftotext vis stdout.

--- race-grid.pl
+++ race-grid.pl
@@ -1,44 +1,53 @@
 #! /usr/bin/env perl
 
-use strict;
+#use strict;
 use warnings;
 
 # parse race grid from text file
 
-my $data_dir  = 'data/src/';
-my $grid_txt  = 'aus-race-grid.txt';
-my $path      = $data_dir . $grid_file;
-my $grid_pdf  = "$ENV{HOME}/Documents/F1/aus-race-grid.pdf";
+my $data_dir = 'data/src/';
+my $grid_txt = 'aus-race-grid.txt';
+my $path     = $data_dir . $grid_txt;
+my $grid_pdf = "$ENV{HOME}/Documents/F1/aus-race-grid.pdf";
 use constant PDFTOTEXT => '/usr/local/bin/pdftotext';
 
-#open GRID, "<", $path
+$name_re    = q#[A-Z]\. [A-Z '-]+#;
+$time_re    = '\d:\d\d\.\d\d\d';
+$entrant_re = '[\w &]+';
+$pos_re     = '\d{1,2}';
+$no_re      = $pos_re;
+
+#open my $grid, "<", $path
 #  or die "Unable to open file $path: $!\n";
+# use to arg open method to get shell redirection to stdout
 
-open GRID, "PDFTOTEXT -layout $grid_pdf - |"
-  or die "unable to open PDFTOTEXT: $!";
+open $grid, "PDFTOTEXT -layout $grid_pdf - |"
+      or die "unable to open PDFTOTEXT: $!";
 
-my $name_re    = q#[A-Z]\. [A-Z '-]+#;
-my $time_re    = '\d:\d\d\.\d\d\d';
-my $entrant_re = '[\w &]+';
-my $pos_re     = '\d{1,2}';
-my $no_re      = $pos_re;
+race_grid($grid);
 
-my $odd  = qr/($pos_re)\s+($no_re) ($name_re)($time_re)?/o;
-my $even = qr/($no_re) ($name_re)($time_re)?\s+($pos_re)/o;
-my ( $pos, $no, $name, $time, $entrant );
+sub race_grid
+{
+    my $text = shift;
 
-while (<GRID>) {
-    if (   ( ( $pos, $no, $name, $time ) = /$odd/ )
-        || ( ( $no, $name, $time, $pos ) = /$even/ ) )
-    {
-        ($entrant) = ( <GRID> =~ /^\s+($entrant_re)/o );
-        $entrant =~ s/\s+$//;
-        $name    =~ s/\s+$//;
-        $time = 'no time ' unless defined $time;
-        print "$pos\t$no\t$name\t$time\t$entrant\n";
+    while (<$text>) {
+
+        my $odd  = qr/($pos_re)\s+($no_re) ($name_re)($time_re)?/o;
+        my $even = qr/($no_re) ($name_re)($time_re)?\s+($pos_re)/o;
+        my ( $pos, $no, $name, $time, $entrant );
+
+        if (   ( ( $pos, $no, $name, $time ) = /$odd/ )
+            || ( ( $no, $name, $time, $pos ) = /$even/ ) )
+        {
+            ($entrant) = ( <$text> =~ /^\s+($entrant_re)/o );
+            $entrant =~ s/\s+$//;
+            $name    =~ s/\s+$//;
+            $time = 'no time ' unless defined $time;
+            print "$pos\t$no\t$name\t$time\t$entrant\n";
+        }
     }
-}
 
-close GRID
-    or die "bad PDFTOTEXT: $! $?";
+    close $text
+      or die "bad PDFTOTEXT: $! $?";
+}
 
