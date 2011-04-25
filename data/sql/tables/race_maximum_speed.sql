cb49fb785ee64496f6f8b2aa4bd10bb0caa98efe
Nigel Garside <nigel.garside@gmail.com>
2011-04-25 18:44:40 +0100

    SQL for maximum speeds tables

---
+++ data/sql/tables/race_maximum_speed.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS race_maximum_speed;
+
+CREATE TABLE race_maximum_speed
+(
+    race_id CHAR(8) NOT NULL,
+    speedtrap INTEGER NOT NULL,
+    pos INTEGER NOT NULL,
+    no INTEGER NOT NULL,
+    driver VARCHAR(25) NOT NULL,
+    kph DECIMAL(4,1) NOT NULL,
+    PRIMARY KEY (race_id, speedtrap, pos),
+    UNIQUE (race_id, speedtrap, no),
+    FOREIGN KEY (race_id) REFERENCES race(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    CHECK (speedtrap IN(1, 2, 3)),
+    CHECK (pos BETWEEN 1 AND 26),
+    CHECK (no BETWEEN 1 AND 26)
+);
+
+CREATE INDEX IF NOT EXISTS race_maximum_speed_race_id_idx ON race_maximum_speed(race_id);
