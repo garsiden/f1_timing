3c03ca8566821344a4c7d68320577c7f332c8966
Nigel Garside <nigel.garside@gmail.com>
2011-04-26 12:03:07 +0100

    Speed trap tables SQL

---
+++ data/sql/tables/race_speed_trap.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS race_speed_trap;
+
+CREATE TABLE race_speed_trap
+(
+    race_id CHAR(8) NOT NULL,
+    pos INTEGER NOT NULL,
+    no INTEGER NOT NULL,
+    driver VARCHAR(25) NOT NULL,
+    kph DECIMAL(4,1) NOT NULL,
+    time_of_day TIME NOT NULL,
+    PRIMARY KEY (race_id, pos),
+    FOREIGN KEY (race_id) REFERENCES race(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+    UNIQUE (race_id, no),
+    CHECK (pos BETWEEN 1 and 26),
+    CHECK (no BETWEEN 1 AND 26)
+);
+
+CREATE INDEX IF NOT EXISTS race_speed_trap_race_id_idx ON race_speed_trap (race_id);
+CREATE INDEX IF NOT EXISTS race_speed_trap_pos_idx ON race_speed_trap (pos);
+CREATE INDEX IF NOT EXISTS race_speed_trap_no_idx ON race_speed_trap (no);
