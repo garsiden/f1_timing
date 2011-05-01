4bd842da24026cb2fd6f41eca7fbadf412c10b9b
Nigel Garside <nigel.garside@gmail.com>
2011-05-01 17:14:03 +0100

    Change table field order to same as source document

--- data/sql/tables/race_lap_analysis.sql
+++ data/sql/tables/race_lap_analysis.sql
@@ -5,6 +5,7 @@ CREATE TABLE race_lap_analysis
     race_id CHAR(8) NOT NULL,
     no INTEGER NOT NULL,
     lap INTEGER NOT NULL,
+    pit CHAR(1),
     time TIME NOT NULL,
     PRIMARY KEY (race_id, no, lap),
     FOREIGN KEY (race_id) REFERENCES race(id)
