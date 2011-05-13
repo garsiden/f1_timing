cde444d208e7e22a98812d560f35bf544544a66c
Nigel Garside <nigel.garside@gmail.com>
2011-05-13 13:55:12 +0100

    Add circuit and grand_prix fields

--- data/sql/tables/race.sql
+++ data/sql/tables/race.sql
@@ -2,16 +2,17 @@ DROP TABLE IF EXISTS race;
 
 CREATE TABLE race
 (
-    id CHAR(8) NOT NULL PRIMARY KEY,
-    round CHAR(2) NOT NULL,
+    id CHAR(8) NOT NULL,
+    rd CHAR(2) NOT NULL,
     date DATE NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    start_time_local DATETIME NOT NULL,
-    start_time_gmt DATETIME NOT NULL,
+    grand_prix VARCHAR(25) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    circuit VARCHAR(50) NOT NULL,
+    start_time_local TIME NOT NULL,
+    start_time_gmt TIME NOT NULL,
     lap_km DECIMAL(4,3) NOT NULL,
     lap_mi DECIMAL(4,3) NOT NULL,
     laps INTEGER NOT NULL,
-    UNIQUE (id, round)
+    PRIMARY KEY (id),
+    UNIQUE (id, rd)
 );
-
-CREATE INDEX IF NOT EXISTS idx_race_round ON race(round);
