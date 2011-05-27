138c329dcce8556c1f9cbe86ada21f2240d211c5
Nigel Garside <nigel.garside@gmail.com>
2011-05-27 13:18:40 +0100

    Test SQL view to calculate total race time in seconds

---
+++ data/sql/views/total_time.sql
@@ -0,0 +1,11 @@
+DROP VIEW IF EXISTS total_time;
+
+CREATE VIEW total_time AS
+
+SELECT count(no) AS laps, no,
+    round(sum((strftime('%J',[time]) - 2451544.5)*86400.0/58),3) AS total 
+FROM race_lap_analysis
+WHERE race_id='tur-2011' AND lap > 1
+GROUP BY no
+ORDER BY laps DESC, total;
+
