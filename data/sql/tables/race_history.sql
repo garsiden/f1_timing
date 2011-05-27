ea7547a3aa7bc0d84e0f6ef0c3538e70c09e2e71
Nigel Garside <nigel.garside@gmail.com>
2011-05-27 13:23:23 +0100

    Table creation SQL for race history (no primary key)

---
+++ data/sql/tables/race_history.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS race_history;
+
+CREATE TABLE race_history
+(
+    race_id CHAR(8),
+    no INTEGER,
+    lap INTEGER,
+    pit CHAR(3),
+    time TIME
+--  PRIMARY KEY (race_id, no, lap),
+--  FOREIGN KEY (race_id) REFERENCES race(id)
+--  ON UPDATE CASCADE
+--  ON DELETE CASCADE
+--  FOREIGN KEY (race_id, no) REFERENCES race_driver(race_id, no)
+--  ON UPDATE CASCADE
+--  ON DELETE CASCADE,
+--  CHECK (no BETWEEN 1 AND 25)
+);
+
+CREATE INDEX IF NOT EXISTS race_history_race_id_idx ON race_history(race_id);
