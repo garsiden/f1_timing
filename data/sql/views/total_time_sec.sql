ce28d4d03a519f4ead6ff84bf3344c7b964d74b3
Nigel Garside <nigel.garside@gmail.com>
2011-06-06 10:00:20 +0100

    Add to view avg and min to total time in seconds

--- data/sql/views/total_time_sec.sql
+++ data/sql/views/total_time_sec.sql
@@ -2,8 +2,10 @@ DROP VIEW IF EXISTS total_time_sec;
 
 CREATE VIEW total_time_sec AS
 
-SELECT race_id, no, count(no) AS laps, round(sum(secs), 3) AS total, avg(secs) AS avg
+SELECT race_id, no, count(no) AS laps,
+round(sum(secs), 3) AS total,
+avg(secs) AS avg,
+round(min(secs), 3) AS fastest
 FROM race_lap_sec
 GROUP BY race_id, no
 ORDER BY race_id, laps DESC, total ASC;
-
