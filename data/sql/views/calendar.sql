711d1caea2439be6f13125ee2e004d6eeb35a22a
Nigel Garside <nigel.garside@gmail.com>
2011-06-17 20:52:53 +0100

    Use changed field names

--- data/sql/views/calendar.sql
+++ data/sql/views/calendar.sql
@@ -2,7 +2,7 @@ DROP VIEW IF EXISTS calendar;
 
 CREATE VIEW calendar AS
 
-SELECT round, strftime('%d-%m-%Y', [date]) AS date, grand_prix,
+SELECT rd, strftime('%d-%m-%Y', [date]) AS date, gp,
 coalesce(strftime('%H:%M', [date] || 'T' || start_time_gmt, 'localtime'), 'TBA') AS start,
 SUBSTR(id, 1, 3) AS id,
 strftime('%Y', [date]) AS season
