6cfbbc8755c852da939b148acd73723264800133
Nigel Garside <nigel.garside@gmail.com>
2013-05-10 15:48:48 +0100

    Add graph for race lap differences for single driver

--- f1graph.pl
+++ f1graph.pl
@@ -3,6 +3,7 @@
 use DBI;
 use Data::Dumper;
 use Chart::Gnuplot;
+use Time::Local;
 #use POSIX qw(strftime);
 
 use strict;
@@ -32,7 +33,10 @@ my %colours = (
     22, "#ED528A", 23, "#F5AAC4",
 );
 
-lap_times_db();
+#lap_times_db();
+race_winner('chn-2013');
+
+race_lap_diff();
 
 # TODO
 # title from database
@@ -106,6 +110,80 @@ TIMES
 
     #plot chart
     $chart->plot2d(@datasets);
+}
+
+sub race_lap_diff
+{
+    my $no = 4;
+    my $font    = 'Monaco, 12';
+    my $dashed  = 'dashed';       # solid|dashed
+    my $race_id = 'chn-2013';
+
+    # race winning time and numer of laps
+    my $win_time = race_winner($race_id);
+
+    # get a driver's lap times for comparison
+    my $times   = <<'TIMES';
+SELECT secs
+FROM race_lap_sec
+WHERE no=? AND race_id=?
+ORDER BY lap
+TIMES
+    
+    my $dbh = $db_session->();
+    my $sth = $dbh->prepare($times);
+    my $time =
+    $dbh->selectcol_arrayref( $sth, { Columns => [1] },
+        ( $no, $race_id ) );
+    
+    # create array ref of lap times differences
+    my $avg = $win_time->{avg};
+    my $d;
+    my $diff = [ map { $d += $_ - $avg } @$time ];
+
+    # create dataset
+    my $ds = Chart::Gnuplot::DataSet->new(
+        xdata    => [ 1 .. scalar @$diff ],
+        ydata    => $diff,
+        title    => sprintf ("Driver %2d", $no), 
+        style    => "lines",
+        color    => $colours{$no},
+        linetype => line_type($no),
+    );
+
+    # get race hash
+    my $race = get_race($race_id);
+
+    # titles for graph and terminal window
+    my $year  = ( localtime $race->{epoch} )[5] + 1900;
+    my $title = "$race->{gp} Grand Prix $year \\nRace Lap Differences";
+    ( my $term_title = $title ) =~ s/\\n/ - /;
+
+    # Create chart object and specify the properties of the chart
+    my $chart = Chart::Gnuplot->new(
+        terminal => qq!aqua title "$term_title" font "$font" $dashed!,
+        title    => $title,
+        ylabel   => "Difference (secs)",
+        xlabel   => "Lap",
+        ytics    => {
+            labelfmt => "%5.3f",
+        },
+        grid => {
+            linetype => 'dash',
+            xlines   => 'off',
+            ylines   => 'on',
+        },
+        yrange => '[] reverse',
+        xrange => [ 1, $race->{laps} ],
+        legend => {
+            position => 'outside',
+            align    => 'left',
+            title    => 'Key',
+        },
+        key => 'font "Monaco, 10"',
+    );
+
+    $chart->plot2d($ds);
 }
 
 # DATABASE
@@ -188,4 +266,27 @@ sub line_type
       : $no > 13 ? 'solid'
       :            'dash';
 
+}
+
+sub race_winner
+{
+    my $race_id = shift;
+    my $dbh = $db_session->();
+
+    my $sql   = <<'SQL';
+SELECT total_time, laps
+FROM race_classification
+WHERE race_id=? AND pos=1
+SQL
+
+    my $win = $dbh->selectrow_hashref($sql, {}, $race_id);
+    my ($h, $m, $s, $f) = $win->{total_time} =~ /(\d):(\d\d):(\d\d)\.(\d{1,3})/;
+    my $secs = ($h * 60 + $m) * 60 + $s + $f / 1_000;
+
+    $win->{secs} = $secs;
+    $win->{avg} = $secs / $win->{laps};
+    #say $secs;
+    #print Dumper $win;
+
+    return $win;
 }
