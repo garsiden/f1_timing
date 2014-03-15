7ca529428f68f4d75ea82c871d741f59ca210cd9
garsiden <nigel.garside@gmail.com>
2014-03-15 18:19:20 +0000

    Add free to air column 'fta' to calendar.

--- data/sql/views/calendar.sql
+++ data/sql/views/calendar.sql
@@ -5,6 +5,7 @@ CREATE VIEW calendar AS
 SELECT rd, strftime('%d-%m-%Y', [date]) AS date, gp,
 coalesce(strftime('%H:%M', [date] || 'T' || start_time_gmt, 'localtime'), 'TBA') AS start,
 SUBSTR(id, 1, 3) AS id,
+CASE WHEN fta='true' THEN 'yes' ELSE '' END AS fta,
 strftime('%Y', [date]) AS season
 FROM race;
 
